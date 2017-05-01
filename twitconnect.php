66005a1b1fb36e5a403be56869f3f7a611317cf3
Andrew MacDonald <andrewmacdonald97@hotmail.com>
2017-05-01 20:14:37 -0300

    Update twitconnect.php

--- twitconnect.php
+++ twitconnect.php
@@ -5,9 +5,9 @@ session_start();
 include "twitteroauth/twitteroauth.php";
 
 $consumer_key = "SBksCQYndNL72pfbkcd7XITbb";
-$consumer_secret = "***REMOVED***";
+$consumer_secret = /*removed*/;
 $access_token = "701437285695410176-FmUrjOvr59dllOdmLgVOtSFaBioS08z";
-$access_token_secret = "***REMOVED***";
+$access_token_secret = /*removed*/;
 
 $twitter = new TwitterOAuth($consumer_key,$consumer_secret,$access_token,$access_token_secret);
 
